04752a9cee1e2bb6d279dd433f659946f14bf9e4
Faith-Muchiri <faith.wambui@student.moringaschool.com>
2023-01-23 18:50:05 +0300

    image upload rails

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,35 @@
+class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:update]
+
+  # GET /api/recipes
+  def index
+    render json: Recipe.all
+  end
+
+  # PUT /api/recipes/1
+#   def update
+#     if @recipe.update(recipe_params)
+#       render json: @recipe
+#     else
+#       render json: @recipe.errors, status: :unprocessable_entity
+#     end
+#   end
+def update
+  if UpdateRecipeService.new(@recipe, recipe_params).call
+    render json: @recipe
+  else
+    render json: @recipe.errors, status: :unprocessable_entity
+  end
+end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_recipe
+      @recipe = Recipe.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def recipe_params
+      params.permit(:title, :description, :instruction, :image)
+    end
+end
---
+++ app/serializers/recipe_serializer.rb
@@ -0,0 +1,7 @@
+class RecipeSerializer < ActiveModel::Serializer
+  attributes :id, :title, :description, :instruction, :updated_at
+
+  def updated_at
+    object.updated_at.to_date
+  end
+end
